788574c6bb6fab0223540887e4af9149469db84a
Anjana Nayak <anjuritik@gmail.com>
2024-08-12 11:55:00 +0000

    ..

--- src/lib/todolist.svelte
+++ src/lib/todolist.svelte
@@ -1,16 +1,57 @@
-<div>
+<style>
+.todo
+{display: grid;
+grid-template-columns: 2rem 1fr 2rem;
+grid-gap:0.5 rem;
+align-items: center;
+margin:0 0 0.5rem 0;
+padding: 0.5rem;
+border-radius: 8px;
+filter: drop-shadow(2px 4px 6px black);
+transform: translate(-1px,-1px);
+transition: filter 0.2s transform 0.2s;
+}
+.todo button{
+    width: 2rem;
+    height: 2rem;
+    background-color: transparent;
+    background-position: 50% 50%;
+    background-repeat: no-repeat;
+}
+.todo input
+{
+flex: 1;
+padding: 0.5em 2em 0.5em 0.8em;
+border-radius: 4px;
+
+}
+.toggle
+{
+    border: 1px solid black;
+    border-radius: 50%;
+    box-sizing: border-box;
+    background-size: 1em auto;
+}
+.text{
+    position: relative;
+    display: flex;
+    align-items: center;
+    flex:1;
+}
+</style>
+<div class="todo">
 <form action="" method="">
-    <input type="hidden" name="done" value="">
-    <button aria-label="Mark done/Not done"> Done/NotDone</button>
+    <input type="text" name="done" value="">
+    <button aria-label="Mark done/Not done" class="toggle"> Done/NotDone</button>
 </form>
 
 <form action="" method="">
     <input type="text" name="done" value="">
-    <button aria-label="Save Todo "> Save</button>
+    <button aria-label="Save Todo "  class="save"> Save</button>
 </form>
 <form action="" method="">
     <input type="text" name="done" value="">
-    <button aria-label="Delete "> Delete</button>
+    <button aria-label="Delete " class="delete"> Delete</button>
 </form>
 
 </div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,13 +6,52 @@ import ToolItem from "$lib/todolist.svelte";
 const pagettle="Todo";
 const tle1="check"
 </script>
+<style>
+    .todos
+    {
+        width: 100%;
+        max-width: 42rem;
+        margin: 4rem auto 0 auto;
+
+    }
+    .new {
+        margin:0 0 0.5rem 0;
+    }
+    .new input
+    {
+
+        font-size: 28px;
+        width: 100%;
+        border-radius: 8px;
+        padding: 0.5rem 1rem 0.3rem 1rem ;
+        box-sizing: border-box;
+        text-align: center;
+
+    }
+    .todos :global(input)
+    {border: 1px solid transparent;}
+    .todos :global(input:focus-visible)
+    {
+        box-shadow: inset 1px 1px 6px black;
+        outline: none;
+        border: 1px solid red !important;
+    }
+</style>
 <svelte:head>
-    <title>Todos</title>
+ <title>Todos</title>
 </svelte:head>
-<h1>{pagettle}</h1>
-<form action="" method="">
-    <div  class="todo-container">
-<input type="text" name="text" placeholder="+type tp add a todo">
+<div class="todos">
+    <h1>{pagettle}</h1>
+    <form action="" method="" class="new">
+
+        <input type="text" name="text" placeholder="+type tp add a todo">
+        
+    </form>
+        <ToolItem/>
+    
 </div>
-</form>
-<ToolItem/>
+
+
+
+    
+    
