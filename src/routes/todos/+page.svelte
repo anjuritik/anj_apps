a9178a432d9f04d2a041c882c789301c0fc8304d
Anjana Nayak <anjuritik@gmail.com>
2024-08-13 21:37:20 +0530

    ..

--- src/routes/todos/+page.svelte
+++ src/routes/todos/+page.svelte
@@ -1,2 +1,15 @@
+<script>
+  import { onMount } from "svelte";
+let products = []
+    onMount(async()=>{
+const x= await (await fetch('//dummyjson.com/products')).json()
+products = x.products
+    })
+</script>
+
 <h2>hello Anjana , good job</h2>
 <h1>Inside todos</h1>
+
+{#each products as product}
+<p>{product.title}</p>
+{/each}
--- src/routes/todosapi/todos.json.ts
+++
@@ -1,7 +0,0 @@
-export const get = () => {
-    console.log("GET request received"); // Log when the API is accessed
-    return {
-        status: 200,
-        body: { message: "hello from the api" }
-    };
-};
